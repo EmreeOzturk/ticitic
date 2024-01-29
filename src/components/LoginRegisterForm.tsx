b8b32335cc336c90ed1e3f9eb090c2f77aebe284
Emre Ozturk <emreeozturk42@gmail.com>
2024-01-29 14:43:32 +0300

    Update LoginRegisterForm styling and input fields

--- src/components/LoginRegisterForm.tsx
+++ src/components/LoginRegisterForm.tsx
@@ -10,9 +10,9 @@ const LoginRegisterForm: React.FC<LoginRegisterFormProps> = ({
     loginOrRegister
 }) => {
     return (
-        <div className="h-screen w-screen bg-slate-600 flex items-center justify-center gap-2">
+        <div className="h-screen w-screen bg-gradient-to-tl from-violet-800 to-fuchsia-200 flex items-center justify-center gap-2">
             <div className="bg-slate-200 rounded-xl p-6 flex flex-col items-center justify-start gap-4 w-1/3">
-                <h3 className="text-center font-semibold text-4xl">Login Form</h3>
+                <h3 className="text-center pb-8 text-3xl">Login Form</h3>
                 <div className="flex w-3/4 rounded-xl items-center justify-center border-[.3px] border-slate-400">
                     <label className="relative inline-block w-full h-10">
                         <input type="checkbox" className="opacity-0 w-0 h-0"
@@ -28,10 +28,11 @@ const LoginRegisterForm: React.FC<LoginRegisterFormProps> = ({
                 </div>
                 <form className="flex flex-col gap-2 p-4 w-2/3">
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" placeholder="Enter username" className="shadow-xl p-4 w-full rounded-md focus:border-cyan-500 focus:ring-cyan-500" />
+                    <input id="username" type="text" placeholder="Enter username" className="shadow-xl px-4 py-3 w-full rounded-md focus:outline-none focus:shadow-fuchsia-400/40" />
                     <label htmlFor="password">Password</label>
-                    <input id="password" type="password" placeholder="Enter password" className="shadow-xl placehoder-red-400 p-4 w-full rounded-md" />
-                    <button type="submit" className="bg-gradient-to-r from-purple-500 to-fuchsia-500 p-2 text-center font-bold text-lg rounded-md mt-4  transition-all ">
+                    <input id="password" type="password" placeholder="Enter password" className="shadow-xl px-4 py-3 w-full rounded-md focus:outline-none focus:shadow-fuchsia-400/40" />
+                    <a href="#" className="text-indigo-400 hover:underline mb-5">Forgot password?</a>
+                    <button type="submit" className="bg-gradient-to-r from-purple-500 to-fuchsia-500 p-2 text-white text-center font-bold text-lg rounded-md mt-4  transition-all ">
                         {loginOrRegister === "login" ? "Login" : "Register"}
                     </button>
                 </form>
